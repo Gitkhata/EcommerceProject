54e77f19cc7a3203cd2434adb311f102727f4845
Jeevan <cyanide595@gmail.com>
2025-05-01 19:17:11 +0545

    refactor: Simplify toString method in Role class to return only the name

--- EcommerceCommon/src/main/java/np/com/jp/ecommerce/common/entity/Role.java
+++ EcommerceCommon/src/main/java/np/com/jp/ecommerce/common/entity/Role.java
@@ -84,10 +84,6 @@ public class Role {
 
     @Override
     public String toString() {
-        return "Role{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                '}';
+        return name;
     }
 }
