c3a84613a1b0ecd97686877b55488149cc2f2a87
Jeevan <cyanide595@gmail.com>
2025-05-01 10:54:49 +0545

    feat: Initialize Ecommerce project structure with frontend and backend modules
    - Added parent POM for the Ecommerce project.
    - Created EcommerceFrontend module with basic Spring Boot setup.
    - Implemented IndexController for handling root and index requests.
    - Added index.html template for the frontend.
    - Configured application properties for the frontend module.
    - Included Maven Wrapper scripts for easier project setup.
    - Established dependencies for Spring Boot and Thymeleaf in the parent POM.
    - Set up application properties for the parent project.

---
+++ EcommerceWebParent/EcommerceBackend/src/main/java/np/com/jp/ecommerce/admin/EcommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package np.com.jp.ecommerce.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBackendApplication.class, args);
+	}
+
+}
