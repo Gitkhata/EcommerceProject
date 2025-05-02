ce367e415b3f3a02044c06dedef18faf3f80780d
Jeevan <cyanide595@gmail.com>
2025-05-02 17:21:35 +0545

    fix: Correct title typo in users.html and add missing import in UserController

--- EcommerceWebParent/EcommerceBackend/src/main/java/np/com/jp/ecommerce/admin/user/UserRepository.java
+++ EcommerceWebParent/EcommerceBackend/src/main/java/np/com/jp/ecommerce/admin/user/UserRepository.java
@@ -2,8 +2,10 @@ package np.com.jp.ecommerce.admin.user;
 
 import np.com.jp.ecommerce.common.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
 }
